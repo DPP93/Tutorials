01b339b967c46a77985e9af6bc13c2cdf45c35d6
Daniel <dpeczek93@gmail.com>
2016-04-16 14:31:21 +0200

    Finished tutorial from simple Spring Boot

---
+++ SpringBoot/src/test/java/pl/dp/tutorials/spring/springboot/HelloControllerIT.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.dp.tutorials.spring.springboot;
+
+import java.net.URL;
+import static org.hamcrest.Matchers.equalTo;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author Daniel
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest({"server.port=0"})
+public class HelloControllerIT {
+    
+    @Value("${local.server.port}")
+    private int port;
+
+	private URL base;
+	private RestTemplate template;
+
+	@Before
+	public void setUp() throws Exception {
+		this.base = new URL("http://localhost:" + port + "/");
+		template = new TestRestTemplate();
+	}
+
+	@Test
+	public void getHello() throws Exception {
+		ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+		assertThat(response.getBody(), equalTo("Greetings traveller"));
+	}
+}
