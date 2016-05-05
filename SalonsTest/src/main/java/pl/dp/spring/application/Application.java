119567c6f960744e172ad19e58d84e453c259e36
DPP <dpeczek93@gmail.com>
2016-05-05 19:14:46 +0200

    Started working on Car Dealers Application

---
+++ SalonsTest/src/main/java/pl/dp/spring/application/Application.java
@@ -0,0 +1,16 @@
+package pl.dp.spring.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dpp on 5/5/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+
+}
